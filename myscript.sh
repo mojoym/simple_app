545d2801ca96dd2431f5fb1c28ff4518719e4173
mojoym <mojoym@gmail.com>
2023-01-05 00:10:17 +0200

    Create myscript.sh

---
+++ myscript.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+// install docker & docker-compose
+
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo apt install docker-compose-plugin
+
+
+// create folder & get content from git
+
+mkdir /home/makar/Documents/docker_content
+cd /home/makar/Documents/docker_content
+git clone http://github.com/mojoym/simple_app
+cd /home/makar/Documents/docker_content/simple_app
+
+
+// build images and run containers
+sudo docker compose build
+sudo docker compose up -d
