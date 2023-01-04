6f9516ad83f7eacb628e63146f5225295b231fb1
mojoym <mojoym@gmail.com>
2023-01-05 01:11:10 +0200

    6

--- myscript.sh
+++ myscript.sh
@@ -7,7 +7,7 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
-sudo apt install docker.io  -y whatever
+sudo apt install docker.io  -y
 
 echo "get content from git"
 
